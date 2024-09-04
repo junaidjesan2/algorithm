8bdbe457d164b178a9394787900f9bc02fd8eaf5
junaidjesan2 <junaidjesan2@gmail.com>
2024-09-05 01:50:52 +0600

    dfs implemented

---
+++ week- 01 (basic graph,bfs & dfs)/dfs and 2d grid/dfs_implementation.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 1e5 + 5;
+vector<int> v[N];
+bool vis[N];
+
+void dfs(int src)
+{
+    cout << src << endl;
+    vis[src] = true;
+    // for (int i = 0; i < v[src].size(); i++)
+    // {
+    //     /* code */
+    // }
+    for (int children : v[src])
+    {
+        if (vis[children] == false)
+        {
+            dfs(children);
+        }
+    }
+}
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+    while (e--)
+    {
+        int a, b;
+        cin >> a >> b;
+        v[a].push_back(b);
+        v[b].push_back(a);
+    }
+    memset(vis, false, sizeof(vis));
+    dfs(0);
+    return 0;
+}
