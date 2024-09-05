82be674ce6347bd3c142d815aecfa5a80ac6fa52
junaidjesan2 <junaidjesan2@gmail.com>
2024-09-05 13:24:40 +0600

    dfs 2d grid implemented

---
+++ week- 01 (basic graph,bfs & dfs)/dfs and 2d grid/dfs_2d_grid.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+char mat[20][20];
+bool vis[20][20];
+vector<pair<int, int>> d = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
+
+void dfsGrid(int si, int sj)
+{
+    cout << si << " " << sj << endl;
+    vis[si][sj] = true;
+    for (int i = 0; i < 4; i++)
+    {
+        int ci = si + d[i].first;
+        int cj = sj + d[i].second;
+        cout << ci << " " << cj << endl;
+    }
+}
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> mat[i][j];
+        }
+    }
+    int si, sj;
+    cin >> si >> sj;
+    memset(vis, false, sizeof(vis));
+    dfsGrid(si, sj);
+    return 0;
+}
