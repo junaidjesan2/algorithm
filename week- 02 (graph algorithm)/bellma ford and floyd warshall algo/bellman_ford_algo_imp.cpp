10afd0d2794319e4a04c7d2083a2102c23f7ed80
junaidjesan2 <junaidjesan2@gmail.com>
2024-09-16 16:49:32 +0600

    done

---
+++ week- 02 (graph algorithm)/bellma ford and floyd warshall algo/bellman_ford_algo_imp.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Edge
+{
+public:
+    int u, v, c;
+    Edge(int u, int v, int c)
+    {
+        this->u = u;
+        this->v = v;
+        this->c = c;
+    }
+};
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+    vector<Edge> EdgeList;
+    while (e--)
+    {
+        int u, v, c;
+        cin >> u >> v >> c;
+        EdgeList.push_back(Edge(u, v, c));
+    }
+
+    return 0;
+}
