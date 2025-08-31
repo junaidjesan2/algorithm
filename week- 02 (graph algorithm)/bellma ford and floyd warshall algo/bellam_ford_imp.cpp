a2a6223d8834489d6d4a2dc641a0161ed3f16407
junaidjesan2 <junaidjesan2@gmail.com>
2025-08-31 19:04:47 +0600

    implemented bellam ford alrorithm

---
+++ week- 02 (graph algorithm)/bellma ford and floyd warshall algo/bellam_ford_imp.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Edge
+{
+public:
+    int a, b, c;
+    Edge(int a, int b, int c)
+    {
+        this->a = a;
+        this->b = b;
+        this->c = c;
+    }
+};
+int dis[1005];
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+    vector<Edge> edge_list;
+    while (e--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        edge_list.push_back(Edge(a, b, c));
+    }
+    for (int i = 0; i < n; i++)
+    {
+        dis[i] = INT_MAX;
+    }
+    dis[0] = 0;
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (auto e : edge_list)
+        {
+            int a = e.a;
+            int b = e.b;
+            int c = e.c;
+            if (dis[a] != INT_MAX && dis[a] + c < dis[b])
+            {
+                dis[b] = dis[a] + c;
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << i << "->" << dis[i] << " ";
+    }
+    return 0;
+}
